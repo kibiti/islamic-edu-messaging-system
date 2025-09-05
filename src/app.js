dd13f3b8206da509669508000b4f8593b271d56b
kibiti <53870783+kibiti@users.noreply.github.com>
2025-09-05 18:30:11 +0300

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,6 @@
+const { sendMessagesToAll } = require('./services/messageScheduler');
+
+// Manual trigger for testing (optional)
+// sendMessagesToAll();
+
+console.log('Islamic Education Messenger started. Scheduled tasks are running...');
